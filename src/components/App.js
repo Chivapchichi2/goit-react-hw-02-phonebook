ba3c86e08ce25770ec186fc5c79e211087349703
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-05-29 01:26:54 +0300

    start setup

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import Container from './Container';
+import Header from './Header';
+
+class App extends Component {
+  
+  render() {
+    
+    return (
+        <Container>
+          <Header />
+        
+        </Container>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Container/Container.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Container.module.css'
+
+const Container = ({ children }) => (
+  <div className={styles.Container}>
+    {children}
+  </div>
+);
+
+Container.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export default Container;
